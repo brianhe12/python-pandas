812a976a10cbc74f2d1444c41504b1b02de0dc9a
Brian He <brian.he17@gmail.com>
2020-11-10 22:03:49 -0500

    10

--- 10.py
+++ 10.py
@@ -2,13 +2,8 @@
 import pandas as pd 
 df = pd.read_excel('coalpublic2013.xlsx')
 
-# Create dataframe
-result_df = pd.DataFrame(columns=['MSHA ID','Labor_Hours'])
+def find(id,df):
+    return df[df['MSHA ID']==id]
 
-d = {"MSHA ID": 1,'Labor_Hours':2}
-d2 = {"MSHA ID": 231,'Labor_Hours':2123}
 
-dataframe = pd.Series(d)
-
-print(dataframe)
-#print(result_df)
+print(find(102901,df))
